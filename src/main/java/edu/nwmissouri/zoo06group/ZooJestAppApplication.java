7ce2dc8e54f8a530487db8d0aeb8496129a23a7b
BHUVAN CHANDRA SARAKAM <s544899@nwmissouri.edu>
2021-10-27 11:39:39 -0500

    Initial commit

---
+++ src/main/java/edu/nwmissouri/zoo06group/ZooJestAppApplication.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo06group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooJestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooJestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nwmissouri/zoo06group/ZooJestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo06group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooJestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
