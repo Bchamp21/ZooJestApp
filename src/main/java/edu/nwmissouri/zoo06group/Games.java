da73a5ea4be5685fd611ab84901783b18df52028
BHUVAN CHANDRA SARAKAM <89426072+Bchamp21@users.noreply.github.com>
2021-11-03 12:33:18 -0500

    Create Games.java

---
+++ src/main/java/edu/nwmissouri/zoo06group/Games.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo06group;
+
+public class Games {
+  
+  int players ;
+  String ticketType ;
+  String resultOfGame ;
+  
+	public static void main(String[] args) {
+	
+	}
+
+}
