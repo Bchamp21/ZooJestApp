e57a012606e3a08ed8ff152573fe5740789244cf
BHUVAN CHANDRA SARAKAM <89426072+Bchamp21@users.noreply.github.com>
2021-11-03 12:42:27 -0500

    Update AdventureRides.java

--- src/main/java/edu/nwmissouri/zoo06group/AdventureRides.java
+++ src/main/java/edu/nwmissouri/zoo06group/AdventureRides.java
@@ -5,6 +5,16 @@ package edu.nwmissouri.zoo06group;
  *
  * @author Bhuvan Chandra Sarakam
  */
-public abstract class AdventureRide implements Runnable {
+public class AdventureRide extends AbstractGame {
     
+    int players;
+    double priceOfTicket;
+    String ticketType;
+    
+    public void resultOfGame(){
+    }
+    
+    public String onlineBooking(){
+    return "Your ticket is booked";
+    }
 }
