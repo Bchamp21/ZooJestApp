22c3781e01ee6f6025880cd174d5c1a56b2af643
Srilekha608 <64957724+Srilekha608@users.noreply.github.com>
2021-11-05 11:46:05 -0500

    Updated the Shooting and climbing files

---
+++ src/main/java/edu/nwmissouri/zoo06group/ShootingArcade.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo06group;
+
+/**
+ *
+ * @author Srilekha Janagam
+ */
+public class ShootingArcade extends AbstractGame{
+    private int players;
+    private int bullets;
+    private String ticketType;
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void speed()
+    {
+        System.out.println("BulletSpeed");
+               
+    }
+    public String onlineBooking(){
+    return "Your ticket is booked";
+    }
+
+    @Override
+    public String resultOfGame(){
+        return "You never Loose";
+    }
+    
+    
+    
+    
+}
