86c5941cf3c2eb3974683b7baada460395db6fd4
InduReddyCh <89427709+InduReddyCh@users.noreply.github.com>
2021-11-05 17:41:51 -0500

    Create PanicRoom.java

---
+++ src/main/java/edu/nwmissouri/zoo06group/PanicRoom.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo06group;
+
+/**
+ *
+ * @author Indu Chinthakuntla
+ */
+public class PanicRoom extends AdventureRides {
+    int numberOfRooms;
+    
+    public boolean darkRoom(){
+        return true;
+    }
+}
