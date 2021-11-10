1437feaa99b710aa3d7057824566a77ef136eb16
InduReddyCh <89427709+InduReddyCh@users.noreply.github.com>
2021-11-09 22:42:02 -0600

    Update PanicRoom.java

--- src/main/java/edu/nwmissouri/zoo06group/PanicRoom.java
+++ src/main/java/edu/nwmissouri/zoo06group/PanicRoom.java
@@ -10,28 +10,47 @@ package edu.nwmissouri.zoo06group;
  */
 public class PanicRoom extends AdventureRides {
     int numberOfRooms;
+    /**
+     * Custom PanicRoom function providing result of game
+     *
+     * @return result of game
+     */
      @Override
     public String resultOfGame(){
         System.out.println("You won the game");
         return "You never Loose";
     }
-    
+    /**
+     * Custom PaniRoom function providing details of tickets
+     *
+     * @return ticket details
+     */
     @Override
     public  String onlineBooking(){
          System.out.println("Welcome to adventure rides");
     return "Your ticket is booked";
     }
-
-    
-    public static boolean darkRoom(){
-        System.out.println("Enter the dark room");
-        return true;
+    /**
+     * Custom PaniRoom function providing darkRoom method
+     *
+     * @return result of dark room
+     */
+     public static boolean darkRoom(){
+     System.out.println("Enter the dark room");
+     return true;
     }
-
+     /**
+     * Custom PaniRoom function getting values from room
+     *
+     * @return number of rooms
+     */
     public int getNumberOfRooms() {
         return numberOfRooms;
     }
-
+    /**
+     * Custom PaniRoom Setting number of rooms
+     *
+     */
     public void setNumberOfRooms(int numberOfRooms) {
         this.numberOfRooms = numberOfRooms;
     }
