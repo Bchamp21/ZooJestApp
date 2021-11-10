edde15dc6b9d0e486f113dcb0a664b5092af7769
HimajaReddyMaddi <89427685+HimajaReddyMaddi@users.noreply.github.com>
2021-11-09 21:53:11 -0600

    Updated SplashMountain.java

--- src/main/java/edu/nwmissouri/zoo06group/SplashMountain.java
+++ src/main/java/edu/nwmissouri/zoo06group/SplashMountain.java
@@ -19,16 +19,32 @@ public class SplashMountain extends WaterRide{
         this.numberOfSeats = numberOfSeats;
     }
    
+    /**
+     * Custom SplashMountain function providing result of game
+     *
+     * @return result of game
+     */
     @Override
     public String resultOfGame(){
         System.out.println("Congratulation in winning the game");
         return "You never Loose";
     }
     
+   /**
+     * Custom JungleCruze function giving details of online booking
+     *
+     * @return Details of booked ticket
+     */
     public String onlineBooking(){
         System.out.println("Please book your tickets online");
         return "Your ticket is booked";
     }
+    
+     /**
+     * Custom JungleCruze function providing speed of game
+     *
+     * @return speed of game
+     */
     public static String speed(){
         System.out.println("You won the game reached the maximum speed of the game");
         return "";
